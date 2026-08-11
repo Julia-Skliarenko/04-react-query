08cbba9946b1fb11726918702632f832a452f6de
Julia-Skliarenko <rygaja15@gmail.com>
2026-08-11 12:01:36 -0500

    4th vrnt

--- src/services/moviesApi.ts
+++ src/services/moviesApi.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import type { MoviesResponse } from '../../types/movie';
+import type { MoviesResponse } from '../types/movie';
 
 const API_KEY = '955dad1e7382dc291ad52a01b8e57e8f';
 
